b30ff9f605994e686ab06d87fa2812079c4360eb
pedroaal <pedroaal04@gmail.com>
2024-02-04 23:18:22 -0500

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { Show } from 'solid-js'
+import { type Component } from 'solid-js'
+import { Route, Router } from '@solidjs/router'
 
+import { ROUTES } from './constants/router'
 import { RoomProvider } from './contexts/RoomContext'
 
 import Room from './pages/Room'
 import Vote from './pages/Vote'
 
-const App = () => {
-  return (
-    <RoomProvider>
-      <div class="container mx-auto">
-        <header class="text-center">GOPOKER</header>
-        <Room />
-        <Vote />
-      </div>
-    </RoomProvider>
-  )
-}
+const App: Component = () => (
+  <RoomProvider>
+    <div class="container mx-auto">
+      <header class="text-center">SUPAPOKER</header>
+      <Router>
+        <Route path={ROUTES.HOME} component={Room} />
+        <Route path={ROUTES.GAME} component={Vote} />
+      </Router>
+    </div>
+  </RoomProvider>
+)
 
 export default App
