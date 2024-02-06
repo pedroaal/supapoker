f20b40dc8e8b697514e6b4c83cd9220b7ac90c7c
pedroaal <pedroaal04@gmail.com>
2024-02-06 10:35:40 -0500

    minor fixes

--- src/constants/game.ts
+++ src/constants/game.ts
@@ -8,5 +8,14 @@ export const METRICS_OPTIONS: IOption[] = [
 ]
 
 export const METRICS = {
-  scrum: [1, 2, 3, 5, 8, 13],
+  scrum: [
+    { label: '0', value: '0' },
+    { label: '1/2', value: '1/2' },
+    { label: '1', value: '1' },
+    { label: '2', value: '2' },
+    { label: '3', value: '3' },
+    { label: '5', value: '5' },
+    { label: '8', value: '8' },
+    { label: '13', value: '13' },
+  ],
 }
