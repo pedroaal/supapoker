2bbd4aa42d0f7aef7962db5200d046b217e608b2
pedroaal <pedroaal04@gmail.com>
2024-02-05 22:05:07 -0500

    add icons

--- src/types/core.ts
+++ src/types/core.ts
@@ -1,4 +1,12 @@
+import { type JSX } from 'solid-js'
+
 export interface IOption {
   label: string
   value: string
+}
+
+export interface IIcon {
+  path: JSX.Element
+  outline: boolean
+  mini: boolean
 }
